0a3d4799f8567d866c5df2a9b00476c629529296
Джон <knopa.durinda@gmail.com>
2023-06-10 21:49:32 +0300

    zad2

---
+++ src/main/java/zad2/Main.java
@@ -0,0 +1,24 @@
+package zad2;
+
+public class Main {
+    public static void main (String[] args) {
+
+        for (int i = 0; i <= 5; i++) {
+            switch (i) {
+                case 1:
+                    System.out.println("Нечетное");
+                    break;
+                case 3:
+                    System.out.println("Нечетное");
+                    break;
+                case 5:
+                    System.out.println("Нечетное");
+                    break;
+                default:
+                    System.out.println("Четное");
+                    break;
+            }
+        }
+    }
+
+}
